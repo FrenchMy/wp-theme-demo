89bda7960ba978ee39b72429a716f3774d707bbf
FrenchMy <1931726@cgmatane.qc.ca>
2022-02-07 16:35:12 -0500

    ajout du fichier thème

---
+++ single.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+
+<h2>Article</h2>
+    
+    <?php if(have_posts( )) : ?>
+
+        <?php while(have_posts( )) : the_post( ); ?>
+
+            <article class="flex">
+                <div class="image-article quart">
+                
+                <?php if(has_post_thumbnail(  )) 
+                {
+                    the_post_thumbnail( );
+                } ?>
+                </div>
+                <div class="texte-article trois-quart">
+                    <h3><?php the_title(  ); ?></h3>
+                    <p><?php the_content(  ); ?></p>
+                    <p>Ce sujet vous interesse? Consultez d'autre articles dans la catégorie <?php the_category(',' ) ?>.</p>
+                </div>
+            </article>
+
+
+    
+<?php endwhile;
+    endif;
+?>
+<?php get_footer(); ?>
